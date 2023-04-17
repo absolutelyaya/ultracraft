4651dff1c1b884ad1db3110c47f76df0c2bddad7
yaya-nyan <70072204+absolutelyaya@users.noreply.github.com>
2023-04-17 21:56:28 +0200

    re-textured Pierce Revolver and added texture animation to shotgun progress on #2

--- src/main/java/absolutelyaya/ultracraft/client/rendering/item/CoreEjectShotgunRenderer.java
+++ src/main/java/absolutelyaya/ultracraft/client/rendering/item/CoreEjectShotgunRenderer.java
@@ -1,7 +1,10 @@
 package absolutelyaya.ultracraft.client.rendering.item;
 
 import absolutelyaya.ultracraft.Ultracraft;
+import absolutelyaya.ultracraft.accessor.WingedPlayerEntity;
+import absolutelyaya.ultracraft.client.GunCooldownManager;
 import absolutelyaya.ultracraft.item.CoreEjectShotgunItem;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
@@ -17,6 +20,13 @@ public class CoreEjectShotgunRenderer extends GeoItemRenderer<CoreEjectShotgunIt
 	public Identifier getTextureLocation(CoreEjectShotgunItem animatable)
 	{
 		//TODO: charge texture animation
+		GunCooldownManager cdm = ((WingedPlayerEntity)MinecraftClient.getInstance().player).getGunCooldownManager();
+		int primaryCD = cdm.getCooldown(animatable, 0);
+		
+		if(primaryCD < 32)
+			return new Identifier(Ultracraft.MOD_ID, "textures/item/core_shotgun1.png");
+		else if(primaryCD < 37)
+			return new Identifier(Ultracraft.MOD_ID, "textures/item/core_shotgun0.png");
 		//float useTime = 1f - (animatable.getMaxUseTime(null) - animatable.getApproxUseTime()) / (float)(animatable.getMaxUseTime(null));
 		//if(useTime > 0.99f)
 		//	return new Identifier(Ultracraft.MOD_ID, "textures/item/pierce_revolver.png");
