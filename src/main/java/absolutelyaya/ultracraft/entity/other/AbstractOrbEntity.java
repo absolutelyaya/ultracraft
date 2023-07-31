152f5cb6baef4fae9161d63947de7ca1ad8c7712
yaya-nyan <70072204+absolutelyaya@users.noreply.github.com>
2023-07-31 20:34:52 +0200

    improved Orbs a tiny bit

--- src/main/java/absolutelyaya/ultracraft/entity/other/AbstractOrbEntity.java
+++ src/main/java/absolutelyaya/ultracraft/entity/other/AbstractOrbEntity.java
@@ -11,6 +11,7 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.particle.BlockStateParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
@@ -47,6 +48,8 @@ public abstract class AbstractOrbEntity extends Entity
 		super.tick();
 		if(age % 50 == 0)
 			getWorld().addParticle(ParticleRegistry.BIG_CIRCLE, getX(), getY() + 0.5, getZ(), 0, 0, 0);
+		if(age % 70 == 0)
+			playSound(SoundEvents.BLOCK_BEACON_AMBIENT, 1f, 1.25f);
 		if(getPos().distanceTo(getBlockPos().toCenterPos()) > 0.05f)
 		{
 			Vec3d pos = getPos().lerp(getBlockPos().toCenterPos(), 0.02f);
@@ -82,6 +85,13 @@ public abstract class AbstractOrbEntity extends Entity
 	public void onPlayerCollision(PlayerEntity player)
 	{
 		super.onPlayerCollision(player);
+		onRemoved();
+		remove(RemovalReason.KILLED);
+	}
+	
+	@Override
+	public void onRemoved()
+	{
 		for (int i = 0; i < 16; i++)
 		{
 			Vec3d dir = new Vec3d(0, 0, 0).addRandom(random, 1f);
@@ -89,7 +99,7 @@ public abstract class AbstractOrbEntity extends Entity
 			getWorld().addParticle(new BlockStateParticleEffect(ParticleTypes.BLOCK, getParticleBlockstate()), getX(), getY(), getZ(), dir.x, dir.y, dir.z);
 		}
 		getWorld().playSound(null, getBlockPos(), SoundRegistry.BARRIER_BREAK.value(), SoundCategory.PLAYERS, 0.9f, 1f);
-		discard();
+		super.onRemoved();
 	}
 	
 	protected BlockState getParticleBlockstate()
