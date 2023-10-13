fb14ae5ef4143d262a59977d29f80905931bdc5c
yaya-nyan <70072204+absolutelyaya@users.noreply.github.com>
2023-10-13 11:47:02 +0200

    added V2 Animations

--- src/main/java/absolutelyaya/ultracraft/client/rendering/entity/machine/V2Model.java
+++ src/main/java/absolutelyaya/ultracraft/client/rendering/entity/machine/V2Model.java
@@ -4,6 +4,8 @@ import absolutelyaya.ultracraft.Ultracraft;
 import absolutelyaya.ultracraft.entity.machine.V2Entity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import software.bernie.geckolib.constant.DataTickets;
 import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
 import software.bernie.geckolib.core.animation.AnimationState;
@@ -27,7 +29,7 @@ public class V2Model extends GeoModel<V2Entity>
 	@Override
 	public Identifier getAnimationResource(V2Entity animatable)
 	{
-		return /*new Identifier(Ultracraft.MOD_ID, "animations/entities/swordsmachine.animation.json")*/ null;
+		return new Identifier(Ultracraft.MOD_ID, "animations/entities/v2.animation.json");
 	}
 	
 	@Override
@@ -35,6 +37,7 @@ public class V2Model extends GeoModel<V2Entity>
 	{
 		super.setCustomAnimations(animatable, instanceId, animationState);
 		CoreGeoBone head = this.getAnimationProcessor().getBone("head");
+		CoreGeoBone hips = this.getAnimationProcessor().getBone("hips");
 		
 		float f = ((float) Math.PI / 180F);
 		if(MinecraftClient.getInstance().isPaused())
@@ -45,6 +48,20 @@ public class V2Model extends GeoModel<V2Entity>
 		{
 			head.setRotX(extraData.headPitch() * f);
 			head.setRotY(extraData.netHeadYaw() * f);
+		}
+		
+		if(hips != null && animatable.isOnGround() && animatable.getAnimation() == 0)
+		{
+			Vec3d vel = animatable.getVelocity().normalize();
+			if(vel.length() <= 0.05f)
+				return;
+			else
+				hips.setRotY(0f);
+			float targetYaw = -((float)MathHelper.atan2(vel.x, vel.z)) * MathHelper.DEGREES_PER_RADIAN;
+			float phi = Math.abs(targetYaw - animatable.getYaw()) % 360;
+			float angleDelta = phi > 180 ? 360 - phi : phi;
+			double max = Math.toRadians(60);
+			hips.setRotY(-(float)MathHelper.clamp(Math.toRadians(angleDelta), -max, max));
 		}
 	}
 }
--- src/main/java/absolutelyaya/ultracraft/entity/machine/V2Entity.java
+++ src/main/java/absolutelyaya/ultracraft/entity/machine/V2Entity.java
@@ -2,14 +2,16 @@ package absolutelyaya.ultracraft.entity.machine;
 
 import absolutelyaya.ultracraft.accessor.Enrageable;
 import absolutelyaya.ultracraft.accessor.LivingEntityAccessor;
+import absolutelyaya.ultracraft.damage.DamageSources;
 import absolutelyaya.ultracraft.entity.AbstractUltraHostileEntity;
 import absolutelyaya.ultracraft.entity.IAntiCheeseBoss;
 import absolutelyaya.ultracraft.entity.goal.AntiCheeseProximityTargetGoal;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.GoToWalkTargetGoal;
 import net.minecraft.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
@@ -23,15 +25,26 @@ import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache
 import software.bernie.geckolib.core.animation.AnimatableManager;
 import software.bernie.geckolib.core.animation.AnimationController;
 import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.core.animation.RawAnimation;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class V2Entity extends AbstractUltraHostileEntity implements IAntiCheeseBoss, GeoEntity, Enrageable
 {
+	private static final RawAnimation IDLE_ANIM = RawAnimation.begin().thenPlay("idle");
+	private static final RawAnimation WALK_ANIM = RawAnimation.begin().thenPlay("walk");
+	private static final RawAnimation FALL_ANIM = RawAnimation.begin().thenPlay("fall");
+	private static final RawAnimation INTRO_ANIM = RawAnimation.begin().thenPlay("intro").thenPlay("idle");
+	private static final RawAnimation SLIDE_ANIM = RawAnimation.begin().thenPlay("slide_start").thenPlay("slide_loop");
+	private static final RawAnimation SLIDE_END_ANIM = RawAnimation.begin().thenPlay("slide_stop").thenPlay("idle");
 	static protected final TrackedData<Integer> FRUSTRATION = DataTracker.registerData(V2Entity.class, TrackedDataHandlerRegistry.INTEGER);
 	static protected final TrackedData<Integer> DISTANCE = DataTracker.registerData(V2Entity.class, TrackedDataHandlerRegistry.INTEGER);
 	static protected final TrackedData<Integer> IDLE_TIMER = DataTracker.registerData(V2Entity.class, TrackedDataHandlerRegistry.INTEGER);
+	static protected final TrackedData<Integer> INTRO_TICKS = DataTracker.registerData(V2Entity.class, TrackedDataHandlerRegistry.INTEGER);
 	private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
+	protected static final byte ANIMATION_IDLE = 0;
+	protected static final byte ANIMATION_INTRO = 1;
+	protected static final byte ANIMATION_SLIDE = 2;
 	
 	public V2Entity(EntityType<? extends HostileEntity> entityType, World world)
 	{
@@ -55,6 +68,18 @@ public class V2Entity extends AbstractUltraHostileEntity implements IAntiCheeseB
 		dataTracker.startTracking(FRUSTRATION, 0);
 		dataTracker.startTracking(DISTANCE, 0);
 		dataTracker.startTracking(IDLE_TIMER, 0);
+		dataTracker.startTracking(INTRO_TICKS, 0);
+	}
+	
+	@Override
+	public void onTrackedDataSet(TrackedData<?> data)
+	{
+		super.onTrackedDataSet(data);
+		if(data.equals(INTRO_TICKS) && getAnimation() == ANIMATION_INTRO && dataTracker.get(INTRO_TICKS) >= 90)
+		{
+			dataTracker.set(INTRO_TICKS, -1);
+			dataTracker.set(ANIMATION, ANIMATION_IDLE);
+		}
 	}
 	
 	@Override
@@ -82,6 +107,13 @@ public class V2Entity extends AbstractUltraHostileEntity implements IAntiCheeseB
 		}
 		else if(dataTracker.get(IDLE_TIMER) > 0)
 			dataTracker.set(IDLE_TIMER, 0);
+		if(dataTracker.get(INTRO_TICKS) != -1)
+		{
+			if(isOnGround() && getAnimation() != ANIMATION_INTRO)
+				dataTracker.set(ANIMATION, ANIMATION_INTRO);
+			if(getAnimation() == ANIMATION_INTRO)
+				dataTracker.set(INTRO_TICKS, dataTracker.get(INTRO_TICKS) + 1);
+		}
 	}
 	
 	@Override
@@ -96,15 +128,35 @@ public class V2Entity extends AbstractUltraHostileEntity implements IAntiCheeseB
 		dataTracker.set(FRUSTRATION, 0);
 	}
 	
-	private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> tAnimationState)
+	private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> ctx)
 	{
+		switch(getAnimation())
+		{
+			case ANIMATION_IDLE -> ctx.setAnimation(isOnGround() ? IDLE_ANIM : FALL_ANIM);
+			case ANIMATION_INTRO -> ctx.setAnimation(INTRO_ANIM);
+			case ANIMATION_SLIDE -> ctx.setAnimation(SLIDE_ANIM);
+		}
+		return PlayState.CONTINUE;
+	}
+	
+	private <T extends GeoAnimatable> PlayState legPredicate(AnimationState<T> ctx)
+	{
+		if(getAnimation() == ANIMATION_IDLE && ctx.isMoving())
+		{
+			AnimationController<?> controller = ctx.getController();
+			controller.setAnimationSpeed(1.65f);
+			ctx.setAnimation(WALK_ANIM);
+			return PlayState.CONTINUE;
+		}
 		return PlayState.STOP;
 	}
 	
 	@Override
 	public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar)
 	{
-		controllerRegistrar.add(new AnimationController<>(this, "main", 2, this::predicate));
+		controllerRegistrar.add(
+				new AnimationController<>(this, "main", 2, this::predicate),
+				new AnimationController<>(this, "legs", 2, this::legPredicate));
 	}
 	
 	@Override
@@ -135,6 +187,21 @@ public class V2Entity extends AbstractUltraHostileEntity implements IAntiCheeseB
 	public boolean isFireImmune()
 	{
 		return true;
+	}
+	
+	boolean isPlayingIntro()
+	{
+		return dataTracker.get(INTRO_TICKS) > 0;
+	}
+	
+	@Override
+	public boolean damage(DamageSource source, float amount)
+	{
+		if(isPlayingIntro() && !source.isOf(DamageSources.COIN_PUNCH))
+			return false;
+		if(source.isOf(DamageTypes.FALL))
+			return false;
+		return super.damage(source, amount);
 	}
 	
 	@Override
